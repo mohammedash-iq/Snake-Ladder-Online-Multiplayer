552d9279f1edabf901010f9bc8d8fb962e769fce
mohammedash-iq <iammohammedashiq@gmail.com>
2026-09-08 21:33:47 +0530

    fixed the frontend ui look , made it look polished

--- frontend/src/pages/Arena.tsx
+++ frontend/src/pages/Arena.tsx
@@ -1,23 +1,13 @@
-import { useUpdates } from "../store/connectionStore"
 import Board from "../components/Board"
-import GameDetails from "../components/GameDetails"
-
 
 function Arena() {
-    const gameUpdates = useUpdates((state) => state.gameUpdates);
     return (
-        <>
-            <div className="flex gap-6 justify-center items-center">
-                <div className="mt-4">
-                    <Board></Board>
-                </div >
-                <div >
-                    <GameDetails></GameDetails>
-                </div>
-            </div >
-            <div className="text-md font-bold text-gray-800 text-center m-4">{gameUpdates}</div>
-        </>
+        <div className="flex gap-6 justify-center items-center">
+            <Board></Board>
+        </div >
     )
 }
+
+
 
 export default Arena
